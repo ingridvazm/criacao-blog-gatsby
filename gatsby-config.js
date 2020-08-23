7dd00b0c80e56193cb658f71bf12fb5b856c8840
ingridvazm <ingridvazmartinsti@gmail.com>
2020-08-23 18:55:28 +0100

    cleanup started

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Site Ana Martins`,
+    description: `Esse site é sobre os trabalhos de Ana Martins`,
+    author: `@ingridvaz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
